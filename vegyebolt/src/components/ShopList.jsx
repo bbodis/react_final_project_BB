8253bfc9408020c5d92bd109fcf8555497e97377
bodis.benjamin07@gmail.com <bodis.benjamin07@gmail.com>
2026-05-30 09:53:23 +0200

    ShopList javítása

--- vegyebolt/src/components/ShopList.jsx
+++ vegyebolt/src/components/ShopList.jsx
@@ -1,9 +1,9 @@
 import ShopItem from "./ShopItem";
-const ShopList = ({ Shops, deleteShop }) => {
+const ShopList = ({Shops}) => {
   return (
     <div>
-      {Shops.length == 0 ? <h2>Nincs megjeleníthető utazás</h2> : Shops.map((Shop) => (
-        <ShopItem key={Shop.id} Shop={Shop} deleteShop={deleteShop} />
+      {Shops.length == 0 ? <h2>Nincs megjeleníthető utazás</h2> : Shops.map((Shop, index) => (
+        <ShopItem key={index} name={Shop.name} weight={Shop.weight} category={Shop.category} description={Shop.description}/>
       ))}
     </div>
   );
