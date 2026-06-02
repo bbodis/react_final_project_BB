af4398e8dfe86174265f582b8df4250bc1a45f8d
bbodis <bodis.benjamin07@gmail.com>
2026-06-02 14:14:45 +0200

    designolás

--- vegyebolt/src/components/NavBar.jsx
+++ vegyebolt/src/components/NavBar.jsx
@@ -1,13 +1,19 @@
 import { NavLink } from "react-router"
 import { useAuth } from "../context/loginContext"
 import classes from "./NavBar.module.css"
+
 const NavBar = () => {
-    const {isLogged, logout} = useAuth()
+    const { isLogged, logout } = useAuth()
     return (
-        <nav>
-            <NavLink to='/' className={({isActive}) =>isActive ? classes.active:classes.link}>Listanézet</NavLink>
-            {isLogged && <NavLink to='form' className={({isActive}) =>isActive ? classes.active:classes.link}>Új vásárlás</NavLink>}
-             {!isLogged ? <NavLink to='login' className={({isActive}) =>isActive ? classes.active : classes.link}>Bejelentkezés</NavLink> : <a onClick={logout} className={classes.isActive}>Kijelentkezés</a>}
+        <nav className={classes.navbar}>
+            <div className={classes.spacer}></div>
+            <div className={classes.items}>
+                <NavLink to='/' className={({ isActive }) => isActive ? classes.active : classes.link}>Listanézet</NavLink>
+                {isLogged && <NavLink to='form' className={({ isActive }) => isActive ? classes.active : classes.link}>Új vásárlás</NavLink>}
+            </div>
+            <div className={classes.login}>
+                {!isLogged ? (<NavLink to='login' className={({ isActive }) => isActive ? classes.active : classes.link}>Bejelentkezés</NavLink>) : (<a onClick={logout} className={classes.link} style={{ cursor: 'pointer' }}>Kijelentkezés</a>)}
+            </div>
         </nav>
     )
 }
--- vegyebolt/src/components/ShopList.jsx
+++ vegyebolt/src/components/ShopList.jsx
@@ -2,7 +2,7 @@ import ShopItem from "./ShopItem";
 const ShopList = ({Shops, deleteShop}) => {
   return (
     <div>
-      {Shops.length == 0 ? <h2>Nincs megjeleníthető utazás</h2> : Shops.map((Shop) => (
+      {Shops.length == 0 ? <h2>Nincs megjeleníthető termék</h2> : Shops.map((Shop) => (
         <ShopItem key={Shop.id} id={Shop.id} name={Shop.name} img={Shop.img} price={Shop.price} stock={Shop.stock} category={Shop.category} description={Shop.description} deleteShop={deleteShop}/>
       ))}
     </div>
